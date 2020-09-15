60fac538b44d23b906418ab1e9d8a9d0e6d8c0f7
Carlos Amoros <camorost@gmail.com>
2020-09-14 22:11:48 -0500

    big bang

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Nav from './components/nav.svelte';
+    import Button from './components/Button.svelte';
+    let menuItems = [
+        { link: '#', name: 'Docs' },
+        { link: '#', name: 'Home' },
+        { link: '#', name: 'Blog' },
+    ];
+</script>
+
+<Nav {menuItems} />
+<!-- <Button text="Something" /> -->
+<h1>Tutti</h1>
+<p>Veloci</p>
