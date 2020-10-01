1c9a2c1d8b15d385efd4f6ad9474c5ff209f2eb2
Guillermo Antony Cava Nuñez <gcavanunez@gmail.com>
2020-10-01 15:07:06 -0500

    overflow on modal fix and form added the fieldarea component for textareas

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,28 +1,46 @@
 <script>
   import Button from './ui/Button.svelte';
+  import FieldArea from './ui/FieldArea.svelte';
   import { Form, Field, ErrorMessage } from 'svelte-forms-lib';
   import * as yup from 'yup';
   let phone = '51944403320';
 
   const formProps = {
-    initialValues: { name: "", email: "", message: "" },
+    initialValues: { name: '', email: '', message: '' },
     validationSchema: yup.object().shape({
       name: yup.string().required(),
       email: yup
         .string()
         .email()
         .required(),
-      message: yup.string().required()
+      message: yup.string().required(),
     }),
     onSubmit: values => {
       // alert(JSON.stringify(values));
-      let name = `Hola!%0D%0ASoy%20${values.name.split(' ').join('%20')}`;
-      let mail = `mi%20correo%20es%20${values.email}`;
-      let message = `y%20este%20es%20mi%20mensaje:%0D%0A${values.message.split(' ').join('%20')}`;
-      window.open(`https://wa.me/${phone}?text=${name},%20${mail}%20${message}`, '_blank');
-      
-    }
+      const name = `Hola!%0D%0ASoy%20${values.name.split(' ').join('%20')}`;
+      const mail = `mi%20correo%20es%20${values.email}`;
+      // const message = `y%20este%20es%20mi%20mensaje:%0D%0A${values.message.split(' ').join('%20')}`;
+      // window.open(`https://wa.me/${phone}?text=${name},%20${mail}%20${message}`, '_blank');
+
+      console.log('handling whatsapp G version');
+      // check if directo api o wwhatsapp web
+
+      const message = `y%20este%20es%20mi%20mensaje:%0D%0A${values.message.replace(
+        /\n/g,
+        '%0A',
+      )}`;
+      const link = `https://${
+        window.innerWidth < 801 ? 'api' : 'web'
+      }.whatsapp.com/send?phone=${phone}&text=${name},%20${mail}%20${message}`;
+
+      if (window.innerWidth < 801) {
+        window.location.href = link;
+      } else {
+        window.open(link, '_blank');
+      }
+    },
   };
+  console.log(formProps);
 </script>
 
 <Form class="flex flex-col" {...formProps}>
@@ -32,7 +50,16 @@
   <Field class="contact-input" name="email" type="email" placeholder="E-mail" />
   <ErrorMessage name="email" />
 
-  <Field class="pb-12 contact-input-text" name="message" type="text" placeholder="Mensaje" />
+  <!-- <Field
+    class="pb-12 contact-input-text"
+    name="message"
+    type="text"
+    placeholder="Mensaje" /> -->
+  <FieldArea
+    class="pb-12 contact-input-text"
+    name="message"
+    type="text"
+    placeholder="Mensaje" />
   <ErrorMessage name="message" />
   <Button text="Contactar" type="submit" />
   <!-- TODO: Funcionalidad Form -->
---
+++ src/components/ui/FieldArea.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getContext } from 'svelte';
+  // import {key} from './key';
+  import { key } from 'svelte-forms-lib/lib/components/key';
+  export let name;
+  export let type = 'text';
+  const { form, handleChange } = getContext(key);
+</script>
+
+<textarea
+  {name}
+  {type}
+  value={$form[name]}
+  on:change={handleChange}
+  on:blur={handleChange}
+  {...$$props} />
